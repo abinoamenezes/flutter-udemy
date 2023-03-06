bd9772aebec18c1426e6a26cf08fa5c224cf9470
Abinoã Menezes <abinoamenezes89@gmail.com>
2023-03-06 15:39:29 -0300

    fonts

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -12,9 +12,25 @@ class ExpenseApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ThemeData tema = ThemeData();
     return MaterialApp(
       home: MyHomePage(),
-      theme: ThemeData(primaryColor: Colors.purple[400]),
+      theme: tema.copyWith(
+          colorScheme: tema.colorScheme
+              .copyWith(primary: Colors.purple, secondary: Colors.amber),
+          textTheme: tema.textTheme.copyWith(
+            headline6: const TextStyle(
+              fontFamily: 'OpenSans',
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          appBarTheme: const AppBarTheme(
+              titleTextStyle: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold))),
     );
   }
 }
